4408193ba7bd812100ddde83d783677bdc8d24ae
Elias Howell <60593281+ftn806@users.noreply.github.com>
2020-04-26 17:34:33 -0500

    added python3-setuptools to install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Install prerequisites for germination.py
-sudo apt install python3 python3-pip #deb
-sudo dnf install python3 python3-pip #red hat
-sudo pacman -S python3 python3-pip #arch
-pip3 install validators pyquery
+sudo apt install python3 python3-pip python3-setuptools #deb
+sudo dnf install python3 python3-pip python3-setuptools #red hat
+sudo pacman -S python3 python3-pip python3-setuptools #arch
+pip3 install validators pyquery	#python3-pip
