1e74534d20baaddf360b8f862800545619bf1b81
Elias Howell <67905248+ejhpas@users.noreply.github.com>
2020-07-28 22:09:35 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,4 @@
 sudo apt install python3 python3-pip python3-setuptools #deb
 sudo dnf install python3 python3-pip python3-setuptools #red hat
 sudo pacman -S python3 python3-pip python3-setuptools #arch
-#Add FreeBSD prerequisites
 pip3 install validators pyquery	#python3-pip
